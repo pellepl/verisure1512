a2cada0b05e01f8205609332356eb725b44eb778
Peter Andersson <pelleplutt1976@gmail.com>
2015-12-08 10:16:41 +0100

    peter: moar testing

--- peter/src/main.c
+++ peter/src/main.c
@@ -109,24 +109,32 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority) {
   return HAL_OK;
 }
 
-static void MPU_Config (void) {
+/**
+  * @brief  Configure the MPU attributes as Write Through for SRAM1/2.
+  * @note   The Base Address is 0x20010000 since this memory interface is the AXI.
+  *         The Region Size is 256KB, it is related to SRAM1 and SRAM2  memory size.
+  * @param  None
+  * @retval None
+  */
+static void MPU_Config(void)
+{
   MPU_Region_InitTypeDef MPU_InitStruct;
 
   /* Disable the MPU */
   HAL_MPU_Disable();
 
-  /* Configure the MPU attributes for SDRAM */
+  /* Configure the MPU attributes as WT for SRAM */
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.BaseAddress = 0xC0000000;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_4MB;
+  MPU_InitStruct.BaseAddress = 0x20010000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
   MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
   MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
   MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
   MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
   MPU_InitStruct.SubRegionDisable = 0x00;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
 
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
 
@@ -134,15 +142,14 @@ static void MPU_Config (void) {
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 }
 
+
 static void processor_init(void) {
-  // enable caches
+  // enable I&D caches
   SCB_EnableICache();
   SCB_EnableDCache();
 
-  // test for avoiding systemclock_config usagefault hang
-  //MPU_Config();
+  MPU_Config();
 
-  // test for avoiding systemclock_config usagefault hang
   //HAL_Init();
 
   // pump up the jam to 200MHz
