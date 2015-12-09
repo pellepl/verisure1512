d219e29fe09ca6e5f56c90de7ee53ac45dec71f6
Peter Andersson <pelleplutt1976@gmail.com>
2015-12-09 11:16:10 +0100

    peter: enabled display, copied from example to mydir

--- peter/src/main.c
+++ peter/src/main.c
@@ -8,14 +8,21 @@
 #include <target.h>
 #include <uart_driver.h>
 #include <miniutils.h>
-
-static void delay(uint32_t loops);
-static void processor_init(void);
-static void uart_cb(void *arg, u8_t c);
+#include "stm32f7xx_hal.h"
+#include "stm32746g_discovery.h"
+#include "stm32746g_discovery_lcd.h"
+#include "RGB565_480x272.h"
 
 extern uint8_t FLASH_START, FLASH_SIZE, RAM_START, RAM_SIZE;
 
-static void print_build() {
+static void delay(uint32_t loops);
+static void uart_cb(void *arg, u8_t c);
+static void SystemClock_Config(void);
+static void MPU_Config(void);
+static void LCD_Config(void);
+
+
+static void print_hello() {
   print("\nProcessor running...\n\n");
   print("FLASH: 0x%08x -- 0x%08x (%ikB)\n",
       (u32_t)&FLASH_START, (u32_t)&FLASH_START+(u32_t)&FLASH_SIZE, (u32_t)&FLASH_SIZE/1024);
@@ -36,11 +43,39 @@ static void print_build() {
 
 }
 
-int main(void) {
+static void init_processor(void) {
+  // enable I&D caches
+  SCB_EnableICache();
+  SCB_EnableDCache();
+
+  MPU_Config();
+
+  HAL_Init();
+
+  // pump up the jam to 216MHz
+  SystemClock_Config();
+}
+
+static void init_display(void) {
+  RCC_PeriphCLKInitTypeDef  PeriphClkInitStruct;
+
+  /* LCD clock configuration */
+  /* PLLSAI_VCO Input = HSE_VALUE/PLL_M = 1 Mhz */
+  /* PLLSAI_VCO Output = PLLSAI_VCO Input * PLLSAIN = 192 Mhz */
+  /* PLLLCDCLK = PLLSAI_VCO Output/PLLSAIR = 192/5 = 38.4 Mhz */
+  /* LTDC clock frequency = PLLLCDCLK / LTDC_PLLSAI_DIVR_4 = 38.4/4 = 9.6Mhz */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
+  PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;
+  PeriphClkInitStruct.PLLSAI.PLLSAIR = 5;
+  PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_4;
+  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+
+  /* Configure LCD : Only one layer is used */
+  LCD_Config();
+}
+
+static void init_led(void) {
   GPIO_InitTypeDef gpio_init_structure;
-
-  processor_init();
-
   // LED
   __HAL_RCC_GPIOI_CLK_ENABLE();
 
@@ -52,7 +87,10 @@ int main(void) {
   gpio_init_structure.Speed = GPIO_SPEED_FAST;
   //gpio_init_structure.Alternate = 0; // nevermind
   HAL_GPIO_Init(GPIOI, &gpio_init_structure);
+}
 
+static void init_uart(void) {
+  GPIO_InitTypeDef gpio_init_structure;
 
   // UART
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -77,8 +115,19 @@ int main(void) {
   UART_set_callback(_UART(IOSTD), uart_cb, NULL);
 
   NVIC_EnableIRQ(USART6_IRQn);
+}
 
-  print_build();
+int main(void) {
+
+  init_processor();
+
+  init_led();
+
+  init_uart();
+
+  init_display();
+
+  print_hello();
 
   while (1) {
     delay(10000000);
@@ -98,27 +147,28 @@ static void delay(uint32_t loops) {
     ;
 }
 
+
 /**
   * @brief  System Clock Configuration
   *         The system Clock is configured as follow :
   *            System Clock source            = PLL (HSE)
-  *            SYSCLK(Hz)                     = 200000000
-  *            HCLK(Hz)                       = 200000000
+  *            SYSCLK(Hz)                     = 216000000
+  *            HCLK(Hz)                       = 216000000
   *            AHB Prescaler                  = 1
   *            APB1 Prescaler                 = 4
   *            APB2 Prescaler                 = 2
   *            HSE Frequency(Hz)              = 25000000
   *            PLL_M                          = 25
-  *            PLL_N                          = 400
+  *            PLL_N                          = 432
   *            PLL_P                          = 2
-  *            PLL_Q                          = 8
+  *            PLL_Q                          = 9
   *            VDD(V)                         = 3.3
   *            Main regulator output voltage  = Scale1 mode
-  *            Flash Latency(WS)              = 6
+  *            Flash Latency(WS)              = 7
   * @param  None
   * @retval None
   */
-void SystemClock_Config(void)
+static void SystemClock_Config(void)
 {
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_OscInitTypeDef RCC_OscInitStruct;
@@ -130,9 +180,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 400;
+  RCC_OscInitStruct.PLL.PLLN = 432;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 8;
+  RCC_OscInitStruct.PLL.PLLQ = 9;
 
   ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
   if(ret != HAL_OK)
@@ -140,7 +190,7 @@ void SystemClock_Config(void)
     while(1) { ; }
   }
 
-  /* Activate the OverDrive to reach the 200 MHz Frequency */
+  /* Activate the OverDrive to reach the 216 MHz Frequency */
   ret = HAL_PWREx_EnableOverDrive();
   if(ret != HAL_OK)
   {
@@ -154,17 +204,116 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6);
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7);
   if(ret != HAL_OK)
   {
     while(1) { ; }
   }
 }
 
+
 /* Override */
 HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority) {
   // nop
   return HAL_OK;
+}
+
+
+/**
+  * @brief LCD Configuration.
+  * @note  This function Configure tha LTDC peripheral :
+  *        1) Configure the Pixel Clock for the LCD
+  *        2) Configure the LTDC Timing and Polarity
+  *        3) Configure the LTDC Layer 1 :
+  *           - The frame buffer is located at FLASH memory
+  *           - The Layer size configuration : 480x272
+  * @retval
+  *  None
+  */
+static void LCD_Config(void)
+{
+  static LTDC_HandleTypeDef hltdc_F;
+  LTDC_LayerCfgTypeDef      pLayerCfg;
+
+  /* LTDC Initialization -------------------------------------------------------*/
+
+  /* Polarity configuration */
+  /* Initialize the horizontal synchronization polarity as active low */
+  hltdc_F.Init.HSPolarity = LTDC_HSPOLARITY_AL;
+  /* Initialize the vertical synchronization polarity as active low */
+  hltdc_F.Init.VSPolarity = LTDC_VSPOLARITY_AL;
+  /* Initialize the data enable polarity as active low */
+  hltdc_F.Init.DEPolarity = LTDC_DEPOLARITY_AL;
+  /* Initialize the pixel clock polarity as input pixel clock */
+  hltdc_F.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
+
+  /* The RK043FN48H LCD 480x272 is selected */
+  /* Timing Configuration */
+  hltdc_F.Init.HorizontalSync = (RK043FN48H_HSYNC - 1);
+  hltdc_F.Init.VerticalSync = (RK043FN48H_VSYNC - 1);
+  hltdc_F.Init.AccumulatedHBP = (RK043FN48H_HSYNC + RK043FN48H_HBP - 1);
+  hltdc_F.Init.AccumulatedVBP = (RK043FN48H_VSYNC + RK043FN48H_VBP - 1);
+  hltdc_F.Init.AccumulatedActiveH = (RK043FN48H_HEIGHT + RK043FN48H_VSYNC + RK043FN48H_VBP - 1);
+  hltdc_F.Init.AccumulatedActiveW = (RK043FN48H_WIDTH + RK043FN48H_HSYNC + RK043FN48H_HBP - 1);
+  hltdc_F.Init.TotalHeigh = (RK043FN48H_HEIGHT + RK043FN48H_VSYNC + RK043FN48H_VBP + RK043FN48H_VFP - 1);
+  hltdc_F.Init.TotalWidth = (RK043FN48H_WIDTH + RK043FN48H_HSYNC + RK043FN48H_HBP + RK043FN48H_HFP - 1);
+
+  /* Configure R,G,B component values for LCD background color : all black background */
+  hltdc_F.Init.Backcolor.Blue = 0;
+  hltdc_F.Init.Backcolor.Green = 0;
+  hltdc_F.Init.Backcolor.Red = 0;
+
+  hltdc_F.Instance = LTDC;
+
+/* Layer1 Configuration ------------------------------------------------------*/
+
+  /* Windowing configuration */
+  /* In this case all the active display area is used to display a picture then :
+     Horizontal start = horizontal synchronization + Horizontal back porch = 43
+     Vertical start   = vertical synchronization + vertical back porch     = 12
+     Horizontal stop = Horizontal start + window width -1 = 43 + 480 -1
+     Vertical stop   = Vertical start + window height -1  = 12 + 272 -1      */
+  pLayerCfg.WindowX0 = 0;
+  pLayerCfg.WindowX1 = 480;
+  pLayerCfg.WindowY0 = 0;
+  pLayerCfg.WindowY1 = 272;
+
+  /* Pixel Format configuration*/
+  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_RGB565;
+
+  /* Start Address configuration : frame buffer is located at FLASH memory */
+  pLayerCfg.FBStartAdress = (uint32_t)&RGB565_480x272;
+
+  /* Alpha constant (255 == totally opaque) */
+  pLayerCfg.Alpha = 255;
+
+  /* Default Color configuration (configure A,R,G,B component values) : no background color */
+  pLayerCfg.Alpha0 = 0; /* fully transparent */
+  pLayerCfg.Backcolor.Blue = 0;
+  pLayerCfg.Backcolor.Green = 0;
+  pLayerCfg.Backcolor.Red = 0;
+
+  /* Configure blending factors */
+  pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
+  pLayerCfg.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
+
+  /* Configure the number of lines and number of pixels per line */
+  pLayerCfg.ImageWidth  = 480;
+  pLayerCfg.ImageHeight = 272;
+
+  /* Configure the LTDC */
+  if(HAL_LTDC_Init(&hltdc_F) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+
+  /* Configure the Layer*/
+  if(HAL_LTDC_ConfigLayer(&hltdc_F, &pLayerCfg, 1) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
 }
 
 /**
@@ -200,19 +349,6 @@ static void MPU_Config(void)
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 }
 
-
-static void processor_init(void) {
-  // enable I&D caches
-  SCB_EnableICache();
-  SCB_EnableDCache();
-
-  MPU_Config();
-
-  //HAL_Init();
-
-  // pump up the jam to 200MHz
-  SystemClock_Config();
-}
 
 static volatile uint32_t last_irq = 0;
 
