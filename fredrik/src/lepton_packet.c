40394a600130602f3e9180b88b3e2acea214926c
Fredrik Hederstierna <fredrik@hederstierna.com>
2015-12-10 21:18:47 +0100

    rename packet

--- fredrik/src/lepton_packet.c
+++ fredrik/src/lepton_packet.c
@@ -3,12 +3,12 @@
 #include <uart_driver.h>
 #include <miniutils.h>
 
-#include "lepton_driver.h"
+#include "lepton_packet.h"
 
 //------------------------------------------------
 // calculate crc16-ccitt very fast
 // Idea from: http://www.ccsinfo.com/forum/viewtopic.php?t=24977
-static uint16_t lepton_crc16(const uint8_t *buf, uint16_t len) 
+static uint16_t lepton_packet_crc16(const uint8_t *buf, uint16_t len) 
 {
   uint16_t x;
   uint16_t crc = 0;
@@ -30,12 +30,11 @@ int lepton_packet_check_crc(struct lepton_packet_s *packet)
 
   // The 4 most sig bits not relevant for CRC
   p.id[0] = packet->id[0] & 0x0F;
-  uint16_t crc_pkt = LEPTON_GET_CRC(packet);
+  uint16_t crc_pkt = LEPTON_PACKET_GET_CRC(packet);
   // clear CRC when do calc
   p.crc[0] = 0;
   p.crc[1] = 0;
-  uint16_t crc_test = lepton_crc16((uint8_t*)&p,
-                                   sizeof(p));
+  uint16_t crc_test = lepton_packet_crc16((uint8_t*)&p, sizeof(p));
   if (crc_pkt == crc_test) {
     crc_ok = 1;
   }
---
+++ fredrik/src/lepton_packet.h
@@ -0,0 +1,18 @@
+#ifndef _LEPTON_PACKET_H_
+#define _LEPTON_PACKET_H_
+
+#include <stdint.h>
+
+#define LEPTON_PACKET_GET_ID(p)  (uint16_t)(((uint16_t)(p)->id[0]  << 8) | (uint16_t)(p)->id[1]);
+#define LEPTON_PACKET_GET_CRC(p) (uint16_t)(((uint16_t)(p)->crc[0] << 8) | (uint16_t)(p)->crc[1]);
+
+struct lepton_packet_s
+{
+  uint8_t id[2];
+  uint8_t crc[2];
+  uint8_t payload[160];
+} __attribute__((packed));
+
+int lepton_packet_check_crc(struct lepton_packet_s *packet);
+
+#endif
